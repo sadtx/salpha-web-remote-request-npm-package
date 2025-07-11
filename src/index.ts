d275886972f57ac62d2afd0433d7deb5a2c2e853
kwangsoo8798 <kwangsoo@salphadtx.com>
2025-07-11 17:19:38 +0900

    버전 1.0.6으로 업데이트 및 라이브러리의 기본 진입점 추가

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+// Main entry point for the library
 export { RemoteRequest } from "./remoteRequest";
 export type { RemoteRequestMethod } from "./remoteRequestMehtodType";
+
+// Default export
+export { RemoteRequest as default } from "./remoteRequest";
