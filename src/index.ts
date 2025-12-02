83b9e8e93159a4555329056a2c76a4a24e38527a
kwangsoo8798 <kwangsoo@salphadtx.com>
2025-12-02 10:40:15 +0900

    feat: 1.0.11 TokenTransportType export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 // Main entry point for the library
 export { RemoteRequest } from "./remoteRequest";
 export type { RemoteRequestMethod } from "./remoteRequestMehtodType";
-export type {
-  TokenTransportConfig,
-  TokenTransportType,
-} from "./types/token-trasport-config";
+export type { TokenTransportConfig } from "./types/token-trasport-config";
+export { TokenTransportType } from "./types/token-trasport-config";
 
 // Default export
 export { RemoteRequest as default } from "./remoteRequest";
